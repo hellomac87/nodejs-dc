cbef5f20e34ab6e3fb213f69cd33dbb3e6410932
hellomac87 <hellomac87@gmail.com>
2021-06-02 15:39:17 +0900

    process

---
+++ 1-node/7-process/app.js
@@ -0,0 +1,24 @@
+const process = require("process");
+
+console.log(process.execPath);
+console.log(process.version);
+console.log(process.pid);
+console.log(process.ppid);
+console.log(process.platform);
+console.log(process.env);
+console.log(process.uptime());
+console.log(process.cwd());
+console.log(process.cpuUsage());
+
+setTimeout(() => {
+  console.log("setTimeout");
+}, 0);
+
+// task que 의 제일 앞에 넣어줌
+process.nextTick(() => {
+  console.log("nextTick");
+});
+
+for (let i = 0; i < 100; i++) {
+  console.log("for loop");
+}
