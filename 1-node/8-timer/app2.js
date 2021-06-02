a0c653a67fb7ed45ded239200b416f7ed7095cfc
hellomac87 <hellomac87@gmail.com>
2021-06-02 15:46:07 +0900

    timer, callstack, taskque

---
+++ 1-node/8-timer/app2.js
@@ -0,0 +1,19 @@
+console.log("code1");
+console.time("timeout 0");
+setTimeout(() => {
+  console.timeEnd("timeout 0");
+  console.log("setTimeout 0");
+}, 0);
+
+// for (let i = 0; i < 1000; i++) {}
+
+// console.log("code2");
+// setImmediate(() => {
+//   console.log("setImmediate");
+// });
+
+// console.log("code3");
+// process.nextTick(() => {
+//   // taskQue 에 제일 먼저!
+//   console.log("process.nextTick");
+// });
