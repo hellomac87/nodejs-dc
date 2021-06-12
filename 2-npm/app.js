ff910b0bc9542e1779ed12b0038c50b2bdf7a4f6
hellomac87 <hellomac87@gmail.com>
2021-06-13 00:00:47 +0900

    debugger

--- 2-npm/app.js
+++ 2-npm/app.js
@@ -1,3 +1,27 @@
-console.log("hello");
-console.log("안녕");
 // 💩
+
+function sayHello() {
+  console.log("hello!");
+  console.log("dobby!");
+  console.log("doobby!");
+}
+
+function calculate(x, y) {
+  console.log("calculating..");
+  const result = x + y;
+  sayHello();
+  return result;
+}
+
+calculate(1, 2);
+
+const stop = 4;
+
+console.log(".... looping ....");
+
+for (let i = 0; i < 10; i++) {
+  console.log(`count `, i);
+  if (i === stop) {
+    break;
+  }
+}
