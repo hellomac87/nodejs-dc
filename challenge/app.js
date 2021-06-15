313f7f37fced7b211e7312486daa5ab8877b1745
hellomac87 <hellomac87@gmail.com>
2021-06-15 23:49:38 +0900

    모루게쒀

--- challenge/app.js
+++ challenge/app.js
@@ -3,5 +3,38 @@ const fs = require("fs").promises;
 const folderName = process.argv.find((value) => value === "test");
 
 fs.readdir(`./${folderName}`).then((files) => {
-  console.log(files);
+  const duplicates = [];
+  const videos = [];
+  const captured = [];
+
+  for (const fileName of files) {
+    const extention = fileName.split(".")[1];
+    const isVideo = extention === "mov" || extention === "mp4";
+    const edited = fileName.startsWith("IMG_E");
+
+    if (isVideo) {
+      // video extention
+      videos.push(fileName);
+    } else {
+      // img extention
+      if (extention === "aae" || extention === "png") {
+        captured.push(fileName);
+      }
+      if (!edited) {
+        duplicates.push(fileName);
+      }
+    }
+  }
+
+  console.log({
+    duplicates,
+    videos,
+    captured,
+  });
 });
+
+// 1. 사진 동영상 분리
+// 2. 동영상 video 폴더 mp4 mov
+// 3. png aae => captured
+// 4. E 붙은 (편집된 사진) 그대로 둠
+// 5. 원본사진 => duplicated
