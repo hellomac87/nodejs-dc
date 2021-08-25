fa771f9d49f935a043a757f1c17effa1520b743b
hellomac87 <hellomac87@gmail.com>
2021-08-25 23:27:42 +0900

    router

---
+++ 6-express/app-route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import fs from 'fs';
+import postRouter from './router/post.js';
+import userRouter from './router/user.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/posts', postRouter);
+
+app.use('/users', userRouter);
+
+
+app.listen(8080);
--- 6-express/app.js
+++ 6-express/app.js
@@ -1,11 +1,39 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-});
+app.route('/posts')
+.get((req, res, next) => {
+  res.status(201).send('GET: /posts');
+})
+.post((req, res) => {
+  res.status(201).send('GET: /posts');
+})
+
+app.route('/posts/:id')
+.put((req, res) => {
+  res.status(201).send('PUT: /posts/:id');
+})
+.delete((req,res) => {
+  res.status(201).send('DELETE: /posts/:id');
+})
+
+// app.get('/posts' , (req, res) => {
+//   res.status(201).send('GET: /posts');
+// })
+
+// app.post('/posts' , (req, res) => {
+//   res.status(201).send('POST: /posts');
+// })
+
+// app.put('/posts/:id' , (req, res) => {
+//   res.status(201).send('PUT: /posts/:id');
+// })
+// app.delete('/posts/:id' , (req, res) => {
+//   res.status(201).send('DELETE: /posts/:id');
+// })
 
 app.listen(8080);
