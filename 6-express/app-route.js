a12a280fe2bfea22466b456ee7fae84ca0e1d0ba
hellomac87 <hellomac87@gmail.com>
2021-08-26 23:36:49 +0900

    middlewares

--- 6-express/app-route.js
+++ 6-express/app-route.js
@@ -5,11 +5,23 @@ import userRouter from './router/user.js';
 
 const app = express();
 
-app.use(express.json());
-
+app.use(express.json()); // REST APU, Boby parser
+app.use(express.urlencoded({extended: false})) // HTML Form -> Body
+const options = {
+  dotfiles:'ignore',
+  etag:false,
+  index:false,
+  maxAge:'1d',
+  redirect:false,
+  setHeaders: function (res,path,stat) {
+    res.set('x-timestamp',Date.now())
+  }
+}
+app.use(express.static('public', options));
 app.use('/posts', postRouter);
 
 app.use('/users', userRouter);
+app.get('/index.html')
 
 
 app.listen(8080);
